b2df8fa3722e7ec95d640b250635bf358bd661b0
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-13 16:13:23 +0200

    Save, Load and delete data

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -17,8 +17,41 @@ struct ContentView: View {
     
     // MARK: - Function
     
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return path[0]
+    }
+    
     func save() {
-        dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed!")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                // Do nothing.
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
     }
     
     // MARK: - Body
@@ -29,19 +62,12 @@ struct ContentView: View {
                 TextField("Add New Note", text: $text)
                 
                 Button(action: {
-                    // 1. Only run the action when the text is not empty
                     guard text.isEmpty == false else { return }
                     
-                    // 2. Create a new note item and init it with the text value
                     let note = Note(id: UUID(), text: text)
-                    
-                    // 3. Add the new note to the notes array
                     notes.append(note)
-                    
-                    // 4. Make the text field empty
                     text = ""
                     
-                    // 5. Save the notes (function)
                     save()
                 }, label: {
                     Image(systemName: "plus.circle")
@@ -54,9 +80,35 @@ struct ContentView: View {
             
             Spacer()
             
-            Text("\(notes.count)")
+            if notes.count >= 1 {
+                List {
+                    ForEach(0..<notes.count, id: \.self) { i in
+                        HStack {
+                             Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading, 5)
+                        } // HStack
+                    } // Loop
+                    .onDelete(perform: delete)
+                }
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(25)
+                Spacer()
+            } // List
         } // VStack
         .navigationTitle("Notes")
+        .onAppear(perform: {
+            load()
+        })
     }
 }
 
